e1376b87c52117aa322837cbcac14ee112318541
Kirkwook <benjain.d.kirkwood@gmail.com>
2023-04-19 12:37:14 -0500

    Added dummy screens and basic stack navigator framework

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Home from './screens/home';
+import Navigator from './routes/homeStack';
+
 
 export default function App() {
-  console.log("App Executed");
   return (
-    <View style={styles.container}>
-      <Text>Qlick Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigator/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   console.log("App Executed");
+//   return (
+//     <View style={styles.container}>
+//       <Text>Qlick Hello World!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 
 
