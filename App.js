67bd222fa703016f1782e356a06b7977432b2cc0
Justin Laidlaw <justin.laidlaw102@gmail.com>
2023-04-18 19:45:41 -0500

    Two text boxes and a button for login

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Qlick Hello World!</Text>
-      <StatusBar style="auto" />
+          <StatusBar style="auto" />
+
+          <h1>Welcome to QClick</h1>
+          <div className="login-wrapper">
+              <form>
+                  <label>
+                      <p>Username</p>
+                      <input type="text" />
+                  </label>
+                  <label>
+                      <p>Password</p>
+                      <input type="password" />
+                  </label>
+                  <div>
+                      <button type="submit">Submit</button>
+                  </div>
+              </form>
+          </div>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
