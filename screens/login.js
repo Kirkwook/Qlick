be0da3f807e541ecd693a0bf8abdca38e0e9f8b6
Connor McKeown <106707710+connor-mckeown13@users.noreply.github.com>
2023-04-27 21:01:45 -0500

    added more screens, added components, added global styles

--- screens/login.js
+++ screens/login.js
@@ -1,100 +1,115 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, View, Text, TextInput, Image, Button} from 'react-native';
-import { TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Image,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity
+} from "react-native";
+import { globalStyles } from '../styles/global';
 
 export default function Home({ navigation }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const registerPress = () => {
-        navigation.navigate('Register');
-    }
+  const registerPress = () => {
+    navigation.navigate("Register");
+  };
 
-    const forgotPress = () => {
-        navigation.navigate('ForgotPassword');
-    }
+  const forgotPress = () => {
+    navigation.navigate("ForgotPassword");
+  };
 
-    return (
-        <View style={styles.container}>
-            <Image source = {require("../assets/logo.png")}/>
-            
-        <StatusBar style='auto'/>
+  return (
+    // TouchableWithoutFeedback removes keyboard when tapping anywhere on screen
+    <TouchableWithoutFeedback
+      onPress={() => {
+        console.log("dismissed keyboard");
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={globalStyles.container}>
+        <Image source={require("../assets/logo.png")} />
+
+        <StatusBar style="auto" />
         <View style={styles.inputView}>
-            <TextInput
-                style={styles.TextInput}
-                placeholder="Email"
-                placeholderTextColor="#003f5c"
-                onChangeText={(email) => setEmail(email)}
-            />
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Email"
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setEmail(email)}
+          />
         </View>
 
         <View style={styles.inputView}>
-            <TextInput
-                style={styles.TextInput}
-                placeholder="Password"
-                placeholderTextColor="#003f5c"
-                secureTextEntry={true}
-                onChangeText={(password) => setPassword(password)}
-            />
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password"
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
         </View>
 
         <TouchableOpacity>
-            <Text style={styles.forgot_button} onPress={forgotPress}>Forgot Password?</Text>
+          <Text style={styles.forgot_button} onPress={forgotPress}>
+            Forgot Password?
+          </Text>
         </TouchableOpacity>
 
         <TouchableOpacity>
-            {/* <Text style={styles.forgot_button} onPress={() => navigation.navigate("Register")}>REGISTER</Text> */}
-            <Text style={styles.forgot_button} onPress={registerPress}>REGISTER</Text>
+          {/* <Text style={styles.forgot_button} onPress={() => navigation.navigate("Register")}>REGISTER</Text> */}
+          <Text style={styles.forgot_button} onPress={registerPress}>
+            REGISTER
+          </Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.loginBtn}>
-            <Text style={styles.loginText}>LOGIN</Text>
+          <Text style={styles.loginText}>LOGIN</Text>
         </TouchableOpacity>
-    </View>
-    )
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  image: {
+    marginBottom: 40,
+  },
 
-    image:{
-        marginBottom: 40
-    },
+  inputView: {
+    backgroundColor: "#FFC0CB",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
 
-    inputView: {
-        backgroundColor: "#FFC0CB",
-        borderRadius: 30,
-        width: "70%",
-        height: 45,
-        marginBottom: 20,
-        alignItems: "center",
-    },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
 
-    TextInput: {
-        height: 50,
-        flex: 1,
-        padding: 10,
-        marginLeft: 20,
-    },
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
 
-    forgot_button: {
-        height: 30,
-        marginBottom: 30,
-    },
-
-    loginBtn: {
-        width:"80%",
-        borderRadius:25,
-        height: 50,
-        alignItems:"center",
-        justifyContent:"center",
-        marginTop:40,
-        backgroundColor:"#FF1493",
-    }
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
+  },
 });
