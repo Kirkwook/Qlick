120f783c8b6773b2c9ad37c2abb46790b4abb1a4
Connor McKeown <106707710+connor-mckeown13@users.noreply.github.com>
2023-04-28 01:46:02 -0500

    added a devStack for testing

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Home from './screens/home';
 import LoginStack from './routes/loginStack';
+import DevStack from './routes/devStack';
 
 import { StyleSheet, Text, View } from 'react-native';
 // import { useParams } from "react-router";
