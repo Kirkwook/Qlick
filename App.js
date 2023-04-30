00c167566220ff9837bf533b3949c475f31fc17b
Justin Laidlaw <justin.laidlaw102@gmail.com>
2023-04-30 18:42:53 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,33 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+// import Home from './screens/home';
+import LoginStack from './routes/loginStack';
+import DevStack from './routes/devStack';
+
 import { StyleSheet, Text, View } from 'react-native';
-import { useParams } from "react-router";
-import { Link } from "react-router-dom";
+// import { useParams } from "react-router";
+//import { Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-          <StatusBar style="auto" />
-
-          <h1>Welcome to QClick</h1>
-          <div className="login-wrapper">
-              <form>
-                  <label>
-                      <p>Username</p>
-                      <input type="text" />
-                  </label>
-                  <label>
-                      <p>Password</p>
-                      <input type="password" />
-                  </label>
-                  <div>
-                      <button type="submit">Submit</button>
-                  </div>
-              </form>
-          </div>
-    </View>
+    <LoginStack/>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
