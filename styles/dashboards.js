56497194413fd5bee88063a642f92c4acef7d245
Justin Laidlaw <justin.laidlaw102@gmail.com>
2023-04-30 18:43:15 -0500

    Add files via upload

---
+++ styles/dashboards.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+export const dashboardStyles = StyleSheet.create({
+      buttonContainer: {
+        width: '100%',
+        height: '15%',
+        paddingBottom: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
+      },
+      buttonTitle: {
+        fontSize: 32,
+        color: '#FFFFFF',
+      },
+      buttonText: {
+        width:"100%",
+        height: '100%',
+        borderRadius:75,
+        alignItems:"center",
+        justifyContent:"center",
+        backgroundColor:"#4499FF",
+    },
+
+});
+
+/*
+
+import { dashboardStyles } from '../styles/dashboards';
+to use this sheet.
+
+Make any style notes about additions or color pallettes below:
+
+
+*/
