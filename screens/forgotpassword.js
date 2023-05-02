a1fe91768033f9c15acbe09a570b5aff7419fb9b
BobbyKl <KARPBL5500@uwec.edu>
2023-05-02 14:59:50 -0500

    Added styling

--- screens/forgotpassword.js
+++ screens/forgotpassword.js
@@ -1,16 +1,80 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    Image,
+    TouchableWithoutFeedback,
+    Keyboard,
+    TouchableOpacity,
+    ImageBackground,
+  } from "react-native";
+import { globalStyles } from "../styles/global";
+
+const recover = async () => {
+    /*
+    try {
+
+    } catch (error) {
+      console.error(error);
+    }
+    */
+  };
 
 export default function ForgotPassword() {
+    const [email, setEmail] = useState('');
     return (
-        <View style={styles.container}>
-            <Text>Forgot Password</Text>
+        <View style={globalStyles.container}>
+            <View style={styles.container}>
+                <Text>Forgot Password?</Text>
+            </View>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    placeholderTextColor="#FFF0F0"
+                    value={email}
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                />
+            </View>
+            <TouchableOpacity style={styles.recoverButton}  onPress={recover}>
+              <Text style={globalStyles.loginText}>
+                Recover
+              </Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 40
-    }
+        margin: 10,
+    },
+    input: {
+        width: 300,
+        backgroundColor:"#A2B1C3",
+        color: "#FFFFFF",
+        height: 60,
+        minHeight: 60,
+        borderRadius: 5,
+        marginBottom: 10,
+        fontSize: 18,
+        paddingLeft: 10,
+      },
+    recoverButton: {
+        width: 300,
+        backgroundColor:"#A6C0DF",
+        color: "#FFFFFF",
+        height: 60,
+        minHeight: 60,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginBottom: 10,
+        fontSize: 18,
+        alignItems: "center",
+        justifyContent: "center",
+      },
 });
